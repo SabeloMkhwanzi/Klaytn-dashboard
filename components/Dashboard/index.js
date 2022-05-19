48cb4addabb26d7b737fa88abc38b96e75fba343
sabelo <67804592+SabeloMkhwanzi@users.noreply.github.com>
2022-05-20 01:07:35 +0200

    congfigured volume weight chart component

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -2,41 +2,74 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Box, Text, useColorModeValue } from "@chakra-ui/react";
 import AssetsBox from "../AssetsBox/index";
+import LineCharts from "../LineCharts/index";
 
 function GetApi() {
   const [assets, setAssets] = useState([]);
   const [charts, setCharts] = useState([]);
+  const [volumeWeighted, setVolumeWeighted] = useState([]);
   const InputTextColorMode = useColorModeValue("gray.900", "white");
+  const BoxBgColor = useColorModeValue("#E6E6E6", "#21325E");
 
   useEffect(() => {
     getKlaytnAssets();
     getKlaytnCharts();
+    volumeWeighted;
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const formatCash = (n) => {
+    if (n < 1e3) return n;
+    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1);
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1);
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1);
+    if (n >= 1e12) return +(n / 1e12).toFixed(1);
+  };
+
   const getKlaytnAssets = async (e) => {
     const response = await axios.get("https://api.coincap.io/v2/assets/klaytn");
-    // console.log(response.data);
     setAssets(response.data.data);
-
-    // console.log(assets);
   };
 
   const getKlaytnCharts = async (e) => {
     const response = await axios.get(
-      "https://api.coincap.io/v2/assets/bitcoin/history?interval=h12"
+      "https://api.coincap.io/v2/assets/klaytn/history?interval=d1"
     );
-    // console.log(response.data);
     setCharts(response.data.data);
 
-    // console.log(assets);
+    setVolumeWeighted(
+      charts
+        .map((item) => ({
+          x: new Date(item.date).toLocaleDateString(),
+          y: formatCash(item.priceUsd),
+        }))
+        .reverse()
+    );
   };
 
   return (
     <Box>
-      <Box>{console.log(charts)}</Box>
+      <Box>{console.log(volumeWeighted)}</Box>
 
       <Box mx={3} mb={2}>
+        <Box
+          mx="auto"
+          my={5}
+          //mx={10}
+          px={5}
+          ps={5}
+          py={5}
+          bg={BoxBgColor}
+          rounded="md "
+          borderRadius="lg"
+          h="450px"
+          maxW="950"
+          boxShadow="0px 5px 25px 0px rgba(0, 0, 0, .25);"
+          bgColor="rgba(255, 0, 0, 0.1)"
+        >
+          <LineCharts data={volumeWeighted} />
+        </Box>
         <AssetsBox data={assets} />
       </Box>
     </Box>
--- components/index.js
+++ components/index.js
@@ -4,4 +4,4 @@ export { default as Tokentable } from "./Tokentable";
 export { default as ButtonConnect } from "./ButtonConnect";
 export { default as Navbar } from "./Navbar/index.js";
 export { default as AssetsBox } from "./AssetsBox/index";
-export { default as Charts } from "./Charts/index";
+export { default as LineCharts } from "./LineCharts/index";
